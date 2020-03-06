76a1db6860cea8d1de78e77fae3cfaca128f050c
ilmifirdaus <firdausalya29@gmail.com>
2020-03-06 17:54:19 +0700

    Latihan 1 Intent

--- app/src/main/java/id/putraprima/skorbola/MainActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -1,10 +1,33 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import static android.content.Intent.ACTION_PICK;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView homeLogo, awayLogo;
+    EditText homeName, awayName;
+    Uri imageUri1;
+    Uri imageUri2;
+    Bitmap bitmap1;
+    Bitmap bitmap2;
+    private static final String TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +36,74 @@ public class MainActivity extends AppCompatActivity {
         //TODO
         //Fitur Main Activity
         //1. Validasi Input Home Team
+        homeName = findViewById(R.id.home_team);
         //2. Validasi Input Away Team
-        //3. Ganti Logo Home Team
-        //4. Ganti Logo Away Team
-        //5. Next Button Pindah Ke MatchActivity
+        awayName = findViewById(R.id.away_team);
+
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+
+
+    }
+
+    //3. Ganti Logo Home Team
+    public void handleHomeImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    //4. Ganti Logo Away Team
+    public void handleAwayImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == 1) {
+            if (data != null) {
+                try {
+                    imageUri1 = data.getData();
+                    bitmap1 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri1);
+                    homeLogo.setImageBitmap(bitmap1);
+                } catch (IOException e) {
+                    Toast.makeText(this, "Can't Load Image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        } else if (requestCode == 2) {
+            if (data != null) {
+                try {
+                    imageUri2 = data.getData();
+                    bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri2);
+                    awayLogo.setImageBitmap(bitmap2);
+                } catch (IOException e) {
+                    Toast.makeText(this, "Can't Load Image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+    //5. Next Button Pindah Ke MatchActivity
+    public void handleSubmit(View view) {
+        String homeInput = homeName.getText().toString();
+        String awayInput = awayName.getText().toString();
+        if (!homeInput.equals("") && !awayInput.equals("") && bitmap1 != null && bitmap2 != null) {
+            Intent intent = new Intent(this, MatchActivity.class);
+            intent.putExtra("inputHome", homeInput);
+            intent.putExtra("inputAway", awayInput);
+            intent.putExtra("homeLogo", imageUri1.toString());
+            intent.putExtra("awayLogo", imageUri2.toString());
+
+            startActivity(intent);
+
+        }else {
+            Toast.makeText(this, "Harap isi data terlebih dahulu !!!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,17 +2,93 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MatchActivity extends AppCompatActivity {
+
+    TextView homeName;
+    TextView awayName;
+    TextView awayScore;
+    TextView homeScore;
+    ImageView homeLogo;
+    ImageView awayLogo;
+    Uri uri1;
+    Uri uri2;
+    Bitmap bitmap1;
+    Bitmap bitmap2;
+    String homeTeam;
+    String awayTeam;
+    int home_score = 0;
+    int away_score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        homeName = findViewById(R.id.txt_home);
+        awayName = findViewById(R.id.txt_away);
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+        homeScore = findViewById(R.id.score_home);
+        awayScore = findViewById(R.id.score_away);
+
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
-        //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
-        //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+        Bundle extras = getIntent().getExtras();
+        homeTeam = extras.getString("inputHome");
+        awayTeam = extras.getString("inputAway");
+
+        if(extras != null){
+            uri1 = Uri.parse(extras.getString("homeLogo"));
+            uri2 = Uri.parse(extras.getString("awayLogo"));
+            bitmap1 = null;
+            bitmap2 = null;
+
+            try{
+                bitmap1 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri1);
+                bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri2);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+            homeName.setText(homeTeam);
+            awayName.setText(awayTeam);
+            homeLogo.setImageBitmap(bitmap1);
+            awayLogo.setImageBitmap(bitmap2);
+        }
+
+    }
+
+    //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
+    public void handleHomeScore(View view){
+        home_score = home_score + 1;
+        homeScore.setText(String.valueOf(home_score));
+    }
+    public void handleAwayScore(View view){
+        away_score = away_score + 1;
+        awayScore.setText(String.valueOf(away_score));
+    }
+
+    //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+    public void handleHasil(View view){
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("homeScore", home_score);
+        intent.putExtra("awayScore", away_score);
+        intent.putExtra("homeName", homeTeam);
+        intent.putExtra("awayName", awayTeam);
+
+        startActivity(intent);
     }
 }
